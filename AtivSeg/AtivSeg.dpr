3bb812271ec31a6e4e0e1abe1f7261a08e4ae698
rodrigocolor <coloraodnanet@unochapeco.edu.br>
2015-01-07 21:43:46 -0200

    Revert "Finish ver 1.1"
    This reverts commit 4f548a62f9a6f74ff13ae236fecee8a00748b376.

---
+++ AtivSeg/AtivSeg.dpr
@@ -0,0 +1,15 @@
+program AtivSeg;
+
+uses
+  Vcl.Forms,
+  unAtivacao in 'unAtivacao.pas',
+  unFormAtivacao in 'unFormAtivacao.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
